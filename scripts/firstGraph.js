499cb95e1aa98d96ff5a084eb3d2b79767e463b1
A1pha <majorzhen@gmail.com>
2022-10-04 14:45:43 -0400

    Graphs 1 + some of 2

--- scripts/firstGraph.js
+++ scripts/firstGraph.js
@@ -1,123 +1,164 @@
-const svg = d3.select("svg#svg1");
-const width = svg.attr('width');
-const height = svg.attr('height');
-const margin = { top: 30, right: 30, bottom: 60, left: 60 };
-const chartWidth = width - margin.left - margin.right;
-const chartHeight = height - margin.top - margin.bottom;
+(function () {
+    const svg = d3.select("svg#svg1");
+    const cLegend = d3.select("svg#svg1scale")
 
-let annotations = svg.append("g").attr("id", "annotations");
-let chartArea = svg.append("g").attr("id", "points")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    const width = svg.attr('width');
+    const height = svg.attr('height');
+    const margin = { top: 50, right: 50, bottom: 50, left: 190 };
+    const chartWidth = width - margin.left - margin.right;
+    const chartHeight = height - margin.top - margin.bottom;
 
-d3.json("resources/cleaned.json", d3.autotype).then((data) => {
-    data.forEach(d => {
-        d['Stats'].forEach(d_ => {
-            let freq = d_['freq']
-            d_['Hours_watched'] /= freq
-            d_["Hours_watched"] /= freq
-            d_["Hours_streamed"] /= freq
-            d_["Peak_channels"] /= freq
-            d_["Avg_viewers"] /= freq
-            d_["Genre"] = d.Genre
-            d_["Avg_viewer_ratio"] /= freq
+    let annotations = svg.append("g").attr("id", "annotations");
+    let chartArea = svg.append("g").attr("id", "points")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    d3.json("resources/cleaned.json", d3.autotype).then((data) => {
+
+        data.forEach(d => {
+            d['Stats'].forEach(d_ => {
+                let freq = d_['freq']
+                d_['Hours_watched'] /= freq
+                d_["Hours_watched"] /= freq
+                d_["Hours_streamed"] /= freq
+                d_["Peak_channels"] /= freq
+                d_["Avg_viewers"] /= freq
+                d_["Genre"] = d.Genre
+                d_["Avg_viewer_ratio"] /= freq
+                d_['log_scale_view'] = Math.log10(d_['Avg_viewers']);
+            })
         })
-    })
 
-    console.log(data)
+        let genreList = []
 
-    let smallTrendData = []
-    let largeTrendData = []
-
-    let smallSet = new Set(['Card & Board Game',
-        'Fighting', "Hack and slash\/Beat 'em up", 'Platform', 'Racing'
-        , 'Real Time Strategy (RTS)']);
-
-    data.forEach(d => {
-
-        // if (smallSet.has(d['Genre'])) {
-        //     smallTrendData.push(d)
-        // }
-
-        if (d['Genre'] == 'Role-playing (RPG)' ||
-            d['Genre'] == 'Shooter' ||
-            d['Genre'] == 'Strategy' ||
-            d['Genre'] == 'Simulator') {
-            smallTrendData.push(d);
-        } else {
-            largeTrendData.push(d);
-        }
-    })
-
-    const timeParser = d3.timeParse('%Y-%m-%d');
-
-    let dates = [];
-    let min_value_viewers = Number.MAX_VALUE
-    let max_value_viewers = Number.MIN_VALUE
-    smallTrendData.forEach(d => {
-        d['Stats'].forEach(d_ => {
-            d_['Date'] = timeParser(d_['Date']);
-            dates.push(d_['Date'])
-            min_value_viewers = Math.min(min_value_viewers, d_['Avg_viewers'])
-            max_value_viewers = Math.max(max_value_viewers, d_['Avg_viewers'])
+        data.forEach(d => {
+            genreList.push(d['Genre'])
         })
+
+        const timeParser = d3.timeParse('%Y-%m-%d');
+
+        let dates = [];
+        let views = [];
+        let min_value_viewers = Number.MAX_VALUE
+        let max_value_viewers = Number.MIN_VALUE
+        data.forEach(d => {
+            d['Stats'].forEach(d_ => {
+                d_['Date'] = timeParser(d_['Date']);
+                dates.push(d_['Date'])
+                views.push(d_['log_scale_view'])
+                min_value_viewers = Math.min(min_value_viewers, d_['log_scale_view'])
+                max_value_viewers = Math.max(max_value_viewers, d_['log_scale_view'])
+            })
+        });
+
+        // X axis
+        let timeScale = d3.extent(dates)
+        const dateScale = d3.scaleTime().domain(timeScale).range([0, chartWidth]);
+        let bottomAxis = d3.axisBottom(dateScale).ticks(20)
+        let bottomsubAxis = d3.axisBottom(dateScale).ticks(50).tickFormat("");
+        annotations.append('g')
+            .attr('class', 'x axis')
+            .attr("transform", `translate(${margin.left + 7},${chartHeight + margin.top})`)
+            .call(bottomsubAxis);
+        annotations.append("g")
+            .attr("class", "x axis")
+            .attr("transform", `translate(${margin.left + 7},${chartHeight + margin.top})`)
+            .call(bottomAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-1em")
+            .attr("dy", "0em")
+            .attr("transform", "rotate(-65)");
+
+        // Y Axis
+        const viewerScale = d3.scaleBand().domain(genreList).range([chartHeight, 0]).padding(0.01);
+        let leftAxis = d3.axisLeft(viewerScale)
+        annotations.append("g")
+            .attr("class", "y axis")
+            .attr("transform", `translate(${margin.left},${margin.top})`)
+            .call(leftAxis)
+
+        const colorScale = d3.scaleSequential(d3.interpolateBlues).domain((d3.extent(views)))
+
+        let paths = chartArea.selectAll('g').data(data).join('g')
+            .attr('class', 'tags')
+            .attr('id', d => d.Genre)
+            .style('stroke', d => colorScale(d.log_scale_view));
+
+        paths.selectAll('g')
+            .data(d => d.Stats)
+            .join("rect")
+            .attr("x", d => dateScale(d.Date))
+            .attr("y", d => viewerScale(d.Genre))
+            .attr("width", chartWidth / 72)
+            .attr("height", viewerScale.bandwidth())
+            .style("fill", d => colorScale(d.log_scale_view))
+
+        annotations.raise()
+
+        let legend_data = d3.range(min_value_viewers, max_value_viewers, (max_value_viewers - min_value_viewers) / 50);
+
+        console.log(legend_data)
+
+        let legendColorScale = d3.scaleSequential()
+            .interpolator(d3.interpolateBlues)
+            .domain([min_value_viewers, max_value_viewers]);
+
+        let xColorScale = d3.scaleLinear()
+            .domain([min_value_viewers, max_value_viewers])
+            .range([0, 300]);
+
+        cLegend.selectAll("rect")
+            .data(legend_data)
+            .join("rect")
+            .attr("x", d => Math.floor(xColorScale(d)))
+            .attr("y", 0)
+            .attr("height", 40)
+            .attr("width", d => (Math.floor(xColorScale(d + 1)) - Math.floor(xColorScale(d)) + 1))
+            .attr("fill", d => legendColorScale(d))
+            .attr('transform', `translate(0,${20})`)
+
+        legend_data.forEach((d, i) => {
+            if (i != 0 && i != legend_data.length - 1 && (i % 5 != 0)) {
+                legend_data.splice(i, 1);
+            }
+        });
+
+        cLegend.selectAll("rect").select("text").data(legend_data)
+            .join("text")
+            .text((d, i) => {
+                let temp = Math.pow(10, d);
+                if (i == 0) return "0"
+                if (i == legend_data.length - 1) return Math.trunc(temp.toFixed(4))
+                if (i % 2 == 0) {
+
+                    if (temp < 1000) {
+                        return Math.trunc(temp.toFixed(2))
+                    }
+                    if (temp < 10000) {
+                        return Math.trunc(temp.toFixed(3))
+                    }
+                    return Math.trunc(temp.toFixed(4))
+
+                }
+            })
+            .style("text-anchor", "end")
+            .style("font-size", 10)
+            .attr("x", -45)
+            .attr("y", d => Math.floor(xColorScale(d)))
+            .attr('transform', `translate(0,${20}) rotate(${-90})`)
+
+        cLegend.selectAll("text")
+            .filter(function () {
+                if (d3.select(this).text() == "") {
+                    this.remove()
+                }
+            })
+
+        cLegend.append("text")
+            .text("Key (Corresponds to Avg. Viewers): ")
+            .style("font-size", 10)
+            .attr("x", 0)
+            .attr("y", 10);
     });
-
-    // X axis
-    let timeScale = d3.extent(dates)
-    const dateScale = d3.scaleTime().domain(timeScale).range([0, chartWidth]).nice();
-    let bottomAxis = d3.axisBottom(dateScale).ticks(20)
-    console.log(d3.ticks(timeScale))
-    let bottomGridlines = d3.axisBottom(dateScale)
-        .tickSize(-chartHeight - 10)
-        .tickFormat("")
-
-    annotations.append("g")
-        .attr("class", "x axis")
-        .attr("transform", `translate(${margin.left},${chartHeight + margin.top + 10})`)
-        .call(bottomAxis);
-    annotations.append("g")
-        .attr("class", "x gridlines")
-        .attr("transform", `translate(${margin.left},${chartHeight + margin.top + 10})`)
-        .call(bottomGridlines);
-
-    // Y Axis
-    const viewerScale = d3.scaleLinear().domain([min_value_viewers, max_value_viewers]).range([chartHeight, 0]);
-    let leftAxis = d3.axisLeft(viewerScale).tickFormat(d3.format(",~s"))
-    let leftGridlines = d3.axisLeft(viewerScale)
-        .tickSize(-chartWidth - 10)
-        .tickFormat("")
-    annotations.append("g")
-        .attr("class", "y axis")
-        .attr("transform", `translate(${margin.left - 10},${margin.top})`)
-        .call(leftAxis)
-    annotations.append("g")
-        .attr("class", "y gridlines")
-        .attr("transform", `translate(${margin.left - 10},${margin.top})`)
-        .call(leftGridlines);
-
-    const colorScale = d3.scaleOrdinal(d3.schemeSet2);
-
-    var lineGen = d3.line()
-        .x(d => dateScale(d.Date))
-        .y(d => viewerScale(d.Avg_viewers));
-
-    let paths = chartArea.selectAll('g').data(smallTrendData).join('g')
-        .attr('class', 'tags')
-        .attr('id', d => d.Genre)
-        .style('stroke', d => colorScale(d.Genre));
-
-    paths.append('path').attr('class', 'path')
-        .attr('d', d => lineGen(d.Stats))
-        .style('stroke-width', '2px')
-        .style('fill', 'none');
-
-    chartArea.selectAll('g').selectAll('circle')
-        .data(d => d.Stats)
-        .join('circle')
-        .attr('r', '2px')
-        .attr('cx', d => dateScale(d.Date))
-        .attr('cy', d => viewerScale(d.Avg_viewers))
-        .style('fill', d => colorScale(d.Genre));
-
-});
+})();
 
